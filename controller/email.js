3cca30b6983f30181d874e1b2a99bc9edfc646f9
kirti <kirti@welogicalsolutions.com>
2023-07-06 17:05:03 +0530

    add the express bar plugin

--- controller/email.js
+++ controller/email.js
@@ -5,8 +5,17 @@ require("dotenv").config();
 const appError = require('../utils/appError');
 const catchAsync = require("../utils/catchAsync");
 const nodemailer = require("nodemailer");
-const hbs = require('nodemailer-express-handlebars');
+// const hbs = require('nodemailer-express-handlebars');
 const path = require('path');
+var express = require('express');
+var exphbs  = require('express-handlebars');
+
+var app = express();
+var hbs = exphbs.create({ /* config */ });
+
+// Register `hbs.engine` with the Express app.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 exports.sendingEmail = catchAsync(async (req, res, next) => {
 
