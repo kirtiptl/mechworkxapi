449eacc67ada7434d2df1136dd2f54bc85e05d35
kirti <kirti@welogicalsolutions.com>
2023-07-06 17:15:33 +0530

    comment the email code

--- routes2/v1.0/api/user.js
+++ routes2/v1.0/api/user.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const user = require('../../../controller/user');
 const authorization = require('../../../middleware/auth');
-const email = require("../../../controller/email");
+// const email = require("../../../controller/email");
 
 //USER ROUTE START
 router.post("/singup", user.createUser);
 router.post("/login", user.userLogin);
 router.get("/list", authorization, user.getUsersByUsertype);
-router.post("/send/email", email.sendingEmail);
+// router.post("/send/email", email.sendingEmail);
 //USER ROUTE END
 module.exports = router;
